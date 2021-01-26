d7473e1b5dfa870e3a905513b6aa966046b8e7bd
Abdullah Alqahtani <67982210+xNothing@users.noreply.github.com>
2021-01-27 02:11:56 +0300

    Update Salary-Calculator.py
    The total salary and total expenses have been added, and the remainder of the salary will be shown with SAR and dollars

--- Salary-Calculator.py
+++ Salary-Calculator.py
@@ -1,13 +1,21 @@
 sal = int(input('Enter your salary: '))
-
+sal1 = sal
+Dollar = sal // 3.75
 expenses = {}
-
 for l in range(1, int(input('\nhow much expenses do you have? '))+1):
 	l = input('what is your expenses: ')
 	expenses[l] = 'hi'
 print('\n')	
+total_expenses = 0
 for i in expenses:
 	i = int(input('How much '+str(i)+' price? '))
+	total_expenses = total_expenses + i
 	sal = sal - i
-
-print('\nSalary left: '+str(sal)+'SAR')
+Dollar1 = sal // 3.75
+total_expenses1 = total_expenses // 3.75
+print('\nyour salary: '+str(sal1)+'SAR')
+print('your expenses: '+str(total_expenses)+'SAR')
+print('Salary left: '+str(sal)+'SAR')
+print('\nyour salary with Dollar: '+str(Dollar)+'$')
+print('your expenses: '+str(total_expenses1)+'$')
+print('Salary left: '+str(Dollar1)+'$')
